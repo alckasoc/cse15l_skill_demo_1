3f76cae6d99c3fe46f9acd8e5cf682ba482d04a9
Vincent Tu <tuvincent0106@gmail.com>
2022-02-01 19:35:28 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(10, SkillDemo.multiply(2, 6));
+        assertEquals(10, SkillDemo.multiply(2, 5));
     }
 }
